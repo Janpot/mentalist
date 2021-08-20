cad3e055890ebacb685f6d3c1c0373703e55be66
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-08-20 16:24:52 +0200

    Add some types

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export function has (variable: string): boolean;
+export function number (variable: string, dflt?: number): number;
+export function string (variable: string, dflt?: string): string;
+export function boolean (variable: string, dflt?: boolean): boolean;
